22fe18f133447b1eaf1c918b327c9d39db1f77c1
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-08-19 10:17:46 +0200

    NRP-549 Attempt to execute javascript to click suggestion item.

--- page_object/searchFields.js
+++ page_object/searchFields.js
@@ -42,15 +42,47 @@ var searchCommands = {
             .waitForElementVisible('@searchDestination', timeout)
             .setValue('@searchDestination', destination);
     },
-    enterKeyDestination: function() {
-        this.api.pause(1000);
-        return this.setValue('@searchDestination', this.api.Keys.ENTER);
+    chooseDestination: function(destination) {
+
+        let xpath = "//span[contains(text(), '" + destination + "')]";
+
+
+
+        this.api.useXpath();
+        this.api.waitForElementPresent(xpath, this.api.globals.elementVisibleTimeout);
+
+        this.api.execute(function(destination) {
+          console.log(document);
+
+          var suggestions = document.getElementsByClassName("search-result");
+          console.log("Found " + suggestions.length + " suggestions");
+          for (var i = 0; i < suggestions.length; i++) {
+            // console.log("Comparing " + suggestions[i].innerText);
+            if(suggestions[i].innerText.indexOf(destination) >= 0) {
+              console.log("Found it. Will click it");
+              suggestions[i].click();
+              suggestions[i].parentElement.click();
+              console.log(suggestions[i].parentElement);
+              
+              var e = document.createEvent('HTMLEvents');
+
+              e.initEvent(suggestions[i], 'mousedown', false, true);
+              el.dispatchEvent(e);
+
+              break;
+            }
+          }
+        }, [destination]);
+
+        this.api.useCss();
+        return this;
+
     },
     itinerarySearch: function(origin, destination) {
         return this.setOrigin(origin)
             .enterKeyOrigin()
             .setDestination(destination)
-            .enterKeyDestination();
+            .chooseDestination(destination);
     },
     setSearch: function(search) {
         var timeout = this.api.globals.elementVisibleTimeout;
