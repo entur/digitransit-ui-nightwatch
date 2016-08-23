38de31c0f29e98eca5217ad2f2506c84c7547430
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-08-23 14:22:43 +0200

    NRP-549 Chainify itinerary search

--- tests/itinerary-search/itinerary-search.js
+++ tests/itinerary-search/itinerary-search.js
@@ -1,42 +1,56 @@
 'use strict'
 
 module.exports = {
-    tags: ["itinerary", "skip"],
-    'Origin and destination exists in instructions if suggestion is chosen': function(browser) {
-        var browser = browser.url(browser.launch_url);
-
-        var searchFields = browser.page.searchFields();
-        searchFields.itinerarySearch("Hausmanns gate", "Malerhaugveien 28");
-
-        var itinerarySummary = browser.page.itinerarySummary();
-        itinerarySummary.waitForFirstItineraryRow();
-        itinerarySummary.chooseFirstItinerarySuggestion();
-
-        var itineraryInstructions = browser.page.itineraryInstructions();
-        itineraryInstructions.waitForFirstItineraryInstructionColumn();
-        itineraryInstructions.verifyOrigin("Hausmanns gate, Oslo");
-        itineraryInstructions.verifyDestination("Malerhaugveien 28");
-        browser.end();
+    tags: ["itinerary"],
+    beforeEach: function(browser) {
+      browser.url(browser.launch_url);
+    },
+    'From Hausmanns gate to Malerhaugveien 28': function(browser) {
+        browser.page.searchFields()
+              .itinerarySearch("Hausmanns gate", "Malerhaugveien 28")
+            .api.page.itinerarySummary()
+              .waitForFirstItineraryRow()
+              .chooseFirstItinerarySuggestion()
+            .api.page.itineraryInstructions()
+              .waitForFirstItineraryInstructionColumn()
+              .verifyOrigin("Hausmanns gate, Oslo")
+              .verifyDestination("Malerhaugveien 28")
+            .api.end();
     },
     'From Hausmanns gate to Ula nord': function(browser) {
-        var browser = browser.url(browser.launch_url);
-        var searchFields = browser.page.searchFields();
-        searchFields.itinerarySearch("Hausmanns gate", "Ula nord");
-        browser.page.itinerarySummary().waitForFirstItineraryRow();
-        browser.end();
+        browser.page.searchFields()
+              .itinerarySearch("Hausmanns gate", "Ula nord")
+            .api.page.itinerarySummary()
+              .waitForFirstItineraryRow()
+              .chooseFirstItinerarySuggestion()
+            .api.page.itineraryInstructions()
+              .verifyOrigin("Hausmanns gate, Oslo")
+              .verifyDestination("Ula nord, Larvik")
+            .api.end();
     },
     'From Røros skole to Festplassen, Bergen': function(browser) {
-        var browser = browser.url(browser.launch_url);
-        var searchFields = browser.page.searchFields();
-        searchFields.itinerarySearch("Røros skole", "Festplassen");
-        browser.page.itinerarySummary().waitForFirstItineraryRow();
-        browser.end();
+        browser.page.searchFields()
+            .itinerarySearch("Røros skole", "Festplassen")
+          .api.page.itinerarySummary()
+            .waitForFirstItineraryRow()
+            .chooseFirstItinerarySuggestion()
+          .api.page.itineraryInstructions()
+            .verifyOrigin("Røros skole")
+            .verifyDestination("Festplassen")
+          .api.end();
     },
     'From Festplassen, Bergen to Scandic Alta': function(browser) {
-        var browser = browser.url(browser.launch_url);
-        var searchFields = browser.page.searchFields();
-        searchFields.itinerarySearch("Festplassen", "Scandic Alta");
-        browser.page.itinerarySummary().waitForFirstItineraryRow();
-        browser.end();
+        browser.page.searchFields()
+            .itinerarySearch("Festplassen", "Scandic Alta")
+          .api.page.itinerarySummary()
+            .waitForFirstItineraryRow()
+            .chooseFirstItinerarySuggestion()
+          .api.page.itineraryInstructions()
+            .verifyOrigin("Festplasse")
+            .verifyDestination("Scandic Alta")
+          .api.end();
+    },
+    searchAndVerify: function(browser) {
+      
     }
 }
