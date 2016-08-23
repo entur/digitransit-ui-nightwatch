3874616830513adfcc0f2876d5902596a08c2389
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-08-23 14:24:08 +0200

    NRP-549 Use xpath to find last step in itinerary instructions

--- page_object/itineraryInstructions.js
+++ page_object/itineraryInstructions.js
@@ -2,15 +2,17 @@
 
 var commands = {
     waitForFirstItineraryInstructionColumn: function() {
-        return this.waitForElementVisible("@itineraryInstructionColumn", this.api.globals.itinerarySearchTimeout)
+        return this.waitForElementVisible("@itineraryInstructionColumn", this.api.globals.itinerarySearchTimeout);
     },
     verifyOrigin: function(origin) {
         return this.waitForElementVisible("@itineraryOrigin", this.api.globals.itinerarySearchTimeout)
             .assert.containsText("@itineraryOrigin", origin);
     },
     verifyDestination: function(destination) {
-        return this.waitForElementVisible("@itineraryDestination", this.api.globals.itinerarySearchTimeout)
-            .assert.containsText("@itineraryDestination", destination);
+        this.api.useXpath()
+            .waitForElementVisible(`//div[@class='itinerary-leg-first-row' and contains(text(), ${destination})]`)
+            .useCss();
+        return this;
     }
 };
 
@@ -18,8 +20,6 @@ module.exports = {
     commands: [commands],
     elements: {
         itineraryInstructionColumn: ".itinerary-instruction-column",
-        itineraryOrigin: ".itinerary-leg-first-row:nth-of-type(1)",
-        itineraryDestination: ".itinerary-leg-first-row:last-of-type"
-
+        itineraryOrigin: ".itinerary-leg-first-row:nth-of-type(1)"
     }
 };
