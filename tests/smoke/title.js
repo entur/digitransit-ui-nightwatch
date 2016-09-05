73028e977b89dcaacfd50dea8e390548d3de9ed1
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-09-05 16:33:35 +0200

    NRP-581 Only check that title is not empty

--- tests/smoke/title.js
+++ tests/smoke/title.js
@@ -2,10 +2,12 @@
 
 module.exports = {
     '@tags': ['smoke'],
-    'Page should have title Rutebanken': function(browser) {
+    'Page should have a title': function(browser) {
         browser
             .url(browser.launch_url)
-            .assert.containsText(".title", "Rutebanken")
+            .getText(".title", function(result) {
+              browser.assert.ok(result.value.length > 0);
+            })
             .end();
     }
 };
