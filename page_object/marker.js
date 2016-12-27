fe6951a4ac96c850e715aedf35c1cce26e00cd31
Morten Friesgaard <morten.friesgaard@soprasteria.com>
2016-12-27 20:17:59 +0100

    fixed click on marker

--- page_object/marker.js
+++ page_object/marker.js
@@ -1,22 +1,27 @@
-'use strict'
+'use strict';
 
-var commands = {
+const commands = {
     clickAnyBusStopMarker: function() {
+        this.waitForElementVisible("@anyBusStopMarker");
         return this.click("@anyBusStopMarker");
+    },
+    clickAnyStopMarker: function(mode) {
+        this.waitForElementVisible(`.leaflet-marker-icon.'${mode}'`);
+        return this.click(".leaflet-marker-icon." + mode);
     },
     waitForPopupPaneVisible: function() {
         return this.waitForElementVisible("@popupPane");
     }
-}
+};
 
 module.exports = {
     commands: [commands],
     elements: {
         anyBusStopMarker: {
-            selector: ".leaflet-marker-icon + .bus"
+            selector: ".leaflet-marker-icon.bus"
         },
         popupPane: {
             selector: ".leaflet-pane .leaflet-popup-pane"
         }
     }
-}
+};
