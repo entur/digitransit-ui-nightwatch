7990eb614f634ff28eac3ccc5d557e2d9437a881
Morten Friesgaard <morten.friesgaard@soprasteria.com>
2017-01-19 11:46:05 +0100

    reverted test to show zoom controls on mobil devices

--- tests/custom/zoomControls.js
+++ tests/custom/zoomControls.js
@@ -2,22 +2,13 @@
  We have a created a PR (https://github.com/HSLdevcom/digitransit-ui/pull/1059) on this.
  */
 
-const isMobile = require('../util/util').isMobile();
-
 module.exports = {
   tags: ['zoom-controls', 'custom'],
   'Zoom control buttons should be visible': function (browser) {
     var browser = browser.url(browser.launch_url);
 
-    if (isMobile) {
-      console.log("   - testing small device");
-      browser.assert.elementNotPresent('.leaflet-control-zoom-in');
-      browser.assert.elementNotPresent('.leaflet-control-zoom-out');
-    } else {
-      console.log("   - testing large device");
-      browser.waitForElementVisible('.leaflet-control-zoom-in', 1000);
-      browser.waitForElementVisible('.leaflet-control-zoom-out', 1000);
-    }
+    browser.waitForElementVisible('.leaflet-control-zoom-in', 1000);
+    browser.waitForElementVisible('.leaflet-control-zoom-out', 1000);
     browser.end();
   },
 };
