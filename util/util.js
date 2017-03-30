7fe6b1b019a10278aa728e9c7fb928ad8a7dc0ca
Morten Friesgaard <friesgaard@gmail.com>
2017-03-30 08:42:04 +0200

    adds util function to check if is specific browser

--- util/util.js
+++ util/util.js
@@ -14,7 +14,14 @@ function isMobile() {
   return isEnv('mobile')
 }
 
+function isBrowser(api, name) {
+  return api.options.desiredCapabilities.browserName === name;
+}
 
+// must be executed during test
+module.exports.isBrowser = isBrowser;
+
+// must be executed before test
 module.exports.isEnv = isEnv;
 module.exports.isBeta = isBeta;
 module.exports.isMobile= isMobile;
